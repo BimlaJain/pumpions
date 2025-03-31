dd7d76ab8959189eadd1ee973b36c03f5cadc6ce
Bimla-jain-555 <vj13798@gmail.com>
2025-03-31 10:43:27 +0530

    change club section images of border

--- src/components/ClubMembership.jsx
+++ src/components/ClubMembership.jsx
@@ -9,7 +9,7 @@ import { CLUB_DATA, CLUB_DATA2 } from '@/utils/helper'
 
 const ClubMembership = () => {
     return (
-        <div id='roadmap' className='bg-[#28424F] max-sm:-mt-[54px] relative z-10 overflow-hidden'>
+        <div id='roadmap' className='bg-[#28424F] max-sm:-mt-[54px] relative z-10 overflow-hidden max-w-[1920px]'>
             <p className='font-normal md:text-xl text-base text-white pb-5 text-center pt-24'>Community + Funding + Sunset</p>
             <h3 className='font-normal lg:text-4xl text-2xl text-white text-center lg:pb-[125px] pb-8'>SUNSET CLUB MEMBERSHIP PERKS</h3>
 
@@ -41,20 +41,24 @@ const ClubMembership = () => {
                 <div className='hidden md:flex flex-col gap-2 justify-center items-center lg:mr-auto px-4'>
                     <div className="flex gap-2">
                         {CLUB_DATA.map((item, i) => (
-                            <div key={i} className="flex w-[181px] h-[200px] flex-col justify-center p-6 items-center bg-[url('/assets/images/png/border.png')] bg-cover bg-no-repeat group">
-                                <Image src={item.img} alt={item.alt} width={item.width} height={item.height} />
-                                <h3 className='text-xs font-normal pt-[18px] text-center group-hover:text-white'>{item.title}</h3>
-                                <p className='text-[6px] leading-[204%] font-normal pt-1 text-center group-hover:text-white'>{item.discription}</p>
-                            </div>
+                            <div key={i} className='flex  gap-2 justify-between items-center overflow-hidden group'>
+                                <div className="flex w-[181px] h-[200px] flex-col justify-center p-6 items-center transition-all duration-500 ease-linear bg-[url('/assets/images/png/border.png')] bg-cover  group-hover:bg-[url('/assets/images/png/border-orange.png')] bg-no-repeat">
+                                    <Image src={item.img} alt={item.alt} width={item.width} height={item.height} />
+                                    <h3 className='text-xs font-normal pt-[18px] text-center group-hover:text-white'>{item.title}</h3>
+                                    <p className='text-[6px] leading-[204%] font-normal pt-1 text-center group-hover:text-white'>{item.discription}</p>
+                                </div>
+                          </div>
                         ))}
                     </div>
                     <div className="flex gap-2">
                         {CLUB_DATA2.map((item, i) => (
-                            <div key={i} className="flex w-[181px] h-[200px] flex-col justify-center p-6 items-center bg-[url('/assets/images/png/border.png')] bg-cover bg-no-repeat group">
-                                <Image src={item.img} alt={item.alt} width={item.width} height={item.height} />
-                                <h3 className='text-xs font-normal pt-[18px] text-center group-hover:text-white'>{item.title}</h3>
-                                <p className='text-[6px] leading-[204%] font-normal pt-1 text-center group-hover:text-white'>{item.discription}</p>
-                            </div>
+                            <div key={i} className='flex  gap-2 justify-between items-center overflow-hidden group '>
+                                <div className="flex w-[181px] h-[200px] flex-col justify-center p-6 items-center transition-all duration-500 ease-linear bg-[url('/assets/images/png/border.png')] group-hover:bg-[url('/assets/images/png/border-orange.png')] bg-cover bg-no-repeat">
+                                    <Image src={item.img} alt={item.alt} width={item.width} height={item.height} />
+                                    <h3 className='text-xs font-normal pt-[18px] text-center group-hover:text-white'>{item.title}</h3>
+                                    <p className='text-[6px] leading-[204%] font-normal pt-1 text-center group-hover:text-white'>{item.discription}</p>
+                                </div>
+                           </div>
                         ))}
                     </div>
                 </div>
