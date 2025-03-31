d1524272a680fcddfad87ed9437af38dad030fc8
Bimla-jain-555 <vj13798@gmail.com>
2025-03-31 10:30:43 +0530

    change faq size

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -27,13 +27,13 @@ const Faqs = () => {
             <div className="container max-w-[1170px] mx-auto px-4">
                 {faqs.map((question, index) => (
                     <div key={index} className="relative md:pt-3 max-w-[343px] md:max-w-[735px] w-full mx-auto md:pr-11 px-3 pt-2 mb-6">
-                        <div className="absolute inset-0 bg-[url('/assets/images/png/faq-bg.png')] bg-no-repeat bg-cover max-lg:h-[71px] max-sm:h-[33px]"></div>
+                        <div className="absolute inset-0 bg-[url('/assets/images/png/faq-bg.png')] bg-no-repeat bg-cover h-[71px] max-sm:h-[33px]"></div>
                         <button
                             onClick={() => toggleFAQ(index)}
-                            className="relative w-full text-left md:text-base md:pl-10 pl-0 max-lg:pt-1 max-sm:pt-0 cursor-pointer text-[9px] font-semibold flex justify-between items-center uppercase z-10"
+                            className="relative w-full text-left md:text-base md:pl-10 pl-0 max-lg:pt-1 pt-[5px] max-sm:pt-0 cursor-pointer text-[9px] font-semibold flex justify-between items-center uppercase z-10"
                         >
                             {question}
-                            <span className="md:text-2xl text-sm flex justify-center items-center font-normal max-sm:size-[19px] max-lg:size-[41px] cursor-pointer text-white bg-gradient-to-b from-[#E8770E] to-[#FCA32B] rounded-full md:px-3 md:py-2 px-1">
+                            <span className="md:text-2xl text-sm flex justify-center items-center font-normal max-sm:size-[19px] size-[41px] cursor-pointer text-white bg-gradient-to-b from-[#E8770E] to-[#FCA32B] rounded-full md:px-3 md:py-2 px-1">
                                 {openIndex === index ? "-" : "+"}
                             </span>
                         </button>
